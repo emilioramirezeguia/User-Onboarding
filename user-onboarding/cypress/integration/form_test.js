d2df0d2c91c71032a175c40d94e9681746257bee
Emilio Ramirez <emilioramirezeguia@gmail.com>
2020-06-18 21:26:58 -0700

    Finish testing with Cypress

--- user-onboarding/cypress/integration/form_test.js
+++ user-onboarding/cypress/integration/form_test.js
@@ -1,9 +1,13 @@
 describe("User onboarding flow", function () {
 
-    it("Can navigate to the site.", function () {
+    it("Can navigate to site.", function () {
         cy.visit("http://localhost:3000")
 
         cy.url().should("include", "localhost")
+    })
+
+    it("Disables submit button no info filled out.", function () {
+        cy.get("button").should("be.disabled")
     })
 
     it("Can type a name", function () {
@@ -14,13 +18,26 @@ describe("User onboarding flow", function () {
 
     it("Can type an email", function () {
         cy.get(':nth-child(3) > input')
+            .type("lady@gaga.com")
+            .should("have.value", "lady@gaga.com")
     })
 
     it("Can type an password", function () {
         cy.get(':nth-child(4) > input')
+            .type("1234567890")
+            .should("have.value", "1234567890")
     })
 
-    it("Accepts a checked terms of service", function () {
+    it("Can accept terms of service", function () {
         cy.get(':nth-child(5) > input').check()
     })
+
+    it("Enables submit button after info is filled in.", function () {
+        cy.get("button").should("be.enabled")
+    })
+
+    it("Submits filled in info.", function () {
+        cy.get("button").click()
+    })
+
 })
